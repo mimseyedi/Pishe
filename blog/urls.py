15a35caae8c722d2f200e7c41b193462ccf0e873
mimseyedi <mim.seyedi@gmail.com>
2022-04-17 16:40:15 +0430

    creating search field for blog and guide
    create search field with get method in blog home, blog single and guide home

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import blog_home_view, blog_single_view
+from blog.views import blog_home_view, blog_single_view, blog_search_view
 
 urlpatterns = [
     path('', blog_home_view, name="blog"),
     path('<int:post_id>', blog_single_view, name="blog_single"),
     path('blog_category/<str:cat_name>', blog_home_view, name="blog_category"),
     path('blog_tag/<str:tag_name>', blog_home_view, name="blog_tag"),
+    path('search/', blog_search_view, name="blog_search"),
 ]
--- guide/views.py
+++ guide/views.py
@@ -43,3 +43,32 @@ def guide_single_view(request, paper_id, **kwargs):
 
     return render(request, 'guide/guide_single_page.html', context)
 
+
+def guide_search_view(request, **kwargs):
+    papers = Paper.objects.filter(status=1)
+    last_papers = papers[:3]
+    categories = PaperCategory.objects.all().annotate(posts_count=Count('paper'))
+    tags = PaperTag.objects.all()
+
+    if request.method == "GET":
+        if s := request.GET.get('s'):
+            papers = papers.filter(content__contains=s)
+
+    if kwargs.get("cat_name") != None:
+        papers = papers.filter(category__name=kwargs["cat_name"])
+
+    if kwargs.get("tag_name") != None:
+        papers = papers.filter(tag__name=kwargs["tag_name"])
+
+    papers = Paginator(papers, 4)
+    try:
+        page_number = request.GET.get('page')
+        papers = papers.get_page(page_number)
+    except PageNotAnInteger:
+        papers = papers.get_page(1)
+    except EmptyPage:
+        papers = papers.get_page(1)
+
+    context = {"papers": papers, "categories": categories, "last_papers": last_papers, "tags": tags}
+    return render(request, 'guide/guide_home_page.html', context)
+
