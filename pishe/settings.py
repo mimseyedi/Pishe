b74f3bc02e59414172bb6a953b2123391bb2fb50
mimseyedi <mim.seyedi@gmail.com>
2022-09-14 13:29:35 +0430

    fix bug

--- pishe/settings.py
+++ pishe/settings.py
@@ -143,8 +143,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'mim.seyedi@gmail.com'
-EMAIL_HOST_PASSWORD = '04121375Mim@'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'mim.seyedi@gmail.com'
